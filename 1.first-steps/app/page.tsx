bd2c25a039f8b38ffc743570d87414667f52095c
davidreiner89 <reineralayollaury@gmail.com>
2026-05-03 19:26:48 -0500

    cambio home

--- 1.first-steps/app/page.tsx
+++ 1.first-steps/app/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center p-24">
-      <h1 className="text-3xl font-bold ">Hello world {1 + 1}</h1>
-    </div>
+    <main className="min-h-screen flex flex-col items-center justify-center px-6">
+      <div className="text-center space-y-6">
+        <h1 className="text-4xl font-semibold tracking-tight text-zinc-100">
+          Hello world
+        </h1>
+
+        <Link
+          href="/about"
+          className=" inline-block px-5 py-2.5 rounded-xl text-sm font-medium bg-white text-black hover:bg-zinc-200 transition-all duration-200"
+        >
+          About
+        </Link>
+      </div>
+    </main>
   );
 }
